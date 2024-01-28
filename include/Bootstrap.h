086db33e2ab7eb05240150dce5cce24bf58b08a2
rogerw-gh <rogerw-gh@users.noreply.github.com>
2024-01-28 16:10:17 +0000

    Update Bootstrap.h
    Fix case issue with #include ESP8266WiFi.h which aborts when compiling using PlatformIO/VSC on Linux laptop.

--- include/Bootstrap.h
+++ include/Bootstrap.h
@@ -79,7 +79,7 @@ static TelnetSpy *SandT;
     #define FILE_WRITE "w"
     #define FILE_APPEND "a"
 
-    #include <ESP8266Wifi.h>
+    #include <ESP8266WiFi.h>
     #include <ESPAsyncTCP.h>
     #include "ESP8266TimerInterrupt.h"
 
